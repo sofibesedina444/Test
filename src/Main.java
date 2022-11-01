3184f209670e17a47613e6f89cbd8c6649145cae
salva <salvationordeath@gmail.com>
2022-11-01 15:05:08 +0700

    ДЗ 2 - задачи с 4 по 5

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ДЗ 1 - задача 1:");
-        var dog=8;
-        var cat=3.6;
-        var paper=763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        System.out.println("ДЗ 1 - задача 2:");
-        dog+=4;
-        cat+=4;
-        paper+=4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        System.out.println("ДЗ 1 - задача 3:");
-        var dog1=8.0;
-        dog1-=3.5;
-        cat-=1.6;
-        paper-=7639;
-        System.out.println(dog1);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("ДЗ 2 - задача 4:");
+        var friend=19;
+        System.out.println(friend);
+        friend+=2;
+        System.out.println(friend);
+        friend/=7;
+        System.out.println(friend);
+        System.out.println("ДЗ 2 - задача 5:");
+        var frog=3.5;
+        System.out.println(frog);
+        frog*=10;
+        System.out.println(frog);
+        frog/=3.5;
+        System.out.println(frog);
+        frog+=4;
+        System.out.println(frog);
     }
 }
