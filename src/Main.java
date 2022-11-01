81bf31a50e97a7400df7427cfb5001f6fc0c7724
salva <salvationordeath@gmail.com>
2022-11-01 14:58:25 +0700

    ДЗ 1 - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ДЗ 1 - задача 1:");
+        var dog=8;
+        var cat=3.6;
+        var paper=763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("ДЗ 1 - задача 2:");
+        dog+=4;
+        cat+=4;
+        paper+=4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("ДЗ 1 - задача 3:");
+        var dog1=8.0;
+        dog1-=3.5;
+        cat-=1.6;
+        paper-=7639;
+        System.out.println(dog1);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
